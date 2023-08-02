ec20ee10dafb9330d642c012b625c48c25ebd094
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-08-02 18:49:12 +0300

    if else try

--- main.js
+++ main.js
@@ -1,25 +1,64 @@
-let weapons = ['Rock', 'Paper', 'Scissors']
+let weapons = ['rock', 'paper', 'scissors']
 
 function getComputerChoice () {
     const randomChoice = weapons[Math.floor(Math.random()*weapons.length)]
-    return console.log(round)
+    return console.log(randomChoice)
 }
 
+// function playSingleRound (playerSelection, computerSelection) {
+//  switch (playerSelection, computerSelection) {
+//     case ('rock','paper'):
+//     case ('paper','scissors'):
+//     case ('scissors','rock'):
+//         return "You lose!";
+//         break;
+//     case ('rock', 'rock'):
+//     case ('paper','paper'):
+//     case ('scissors','scissors'):
+//         return "Tie!";
+//         break;
+    
+//     case ('paper','rock'):
+//     case ('scissors','paper'):
+//     case ('rock','scissors'):
+//         return "You win!"
+//         break;
+//     default:
+//         return "Wrong weapons"
+//  }
+// }
+
 function playSingleRound (playerSelection, computerSelection) {
- switch (playerSelection, computerSelection) {
-    case ('rock', 'rock'):
-    case ('paper','paper'):
-    case ('scissors','scissors'):
+    if (playerSelection === "rock"&& computerSelection === "rock") {
         return "Tie!";
-        break;
-    case ('rock','paper'):
-    case ('paper','scissors'):
-    case ('scissors','rock'):
-        return "You lose!";
-        break;
-    case ('paper','rock'):
-    case ('scissors','paper'):
-    case ('rock','scissors'):
-        return "You win!"
- }
+    } 
+    else if (playerSelection === "paper" && computerSelection === "paper"){
+        return "Tie!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "scissors"){
+        return "Tie!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You win!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+       return "You lose!";
+    }
+    else if (playerSelection === "scissiors" && computerSelection === "rock"){
+       return "You lose!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+       return "You lose!";
+    } else { return "wrong"}
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playSingleRound(playerSelection, computerSelection));
+// console.log(computerSelection)
