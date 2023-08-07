dadaf0c78bf5106a49b2f49016be1b540f59aba0
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-08-07 17:15:11 +0300

    added description to win/lose

--- main.js
+++ main.js
@@ -33,22 +33,22 @@ function playSingleRound (playerSelection, computerSelection) {
         return "Tie!";
     }
     else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "You win!";
+        return "You win! Rock beats Scissors";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "You win!";
+        return "You win! Scissors beat Paper";
     }
     else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "You win!";
+        return "You win! Paper beats Rock";
     }
     else if (playerSelection === "paper" && computerSelection === "scissors"){
-       return "You lose!";
+       return "You lose! Scissors beat Paper";
     }
     else if (playerSelection === "scissiors" && computerSelection === "rock"){
-       return "You lose!";
+       return "You lose! Rock beats Scissors";
     }
     else if (playerSelection === "rock" && computerSelection === "paper"){
-       return "You lose!";
+       return "You lose! Paper beats Rock";
     } else { return "wrong"}
 }
 
