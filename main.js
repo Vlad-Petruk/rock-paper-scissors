19b62d61753b8665a75da4f2bedc082dec5f544f
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-08-08 12:48:40 +0300

    with css

--- main.js
+++ main.js
@@ -30,30 +30,28 @@ function playSingleRound (playerSelection, computerSelection) {
        return "You lose! Paper beats Rock";
     } else { return "wrong"}
 }
+let container = document.querySelector('main-container');
 
-// const playerSelection = (prompt('Choose your weapon'))
-// const computerSelection = getComputerChoice();
-// console.log(playSingleRound(playerSelection, computerSelection));
-// console.log(computerSelection)
-function game () {
-    let result;
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i=1; i <= 5; i++) {
-        const playerSelection = (prompt('Choose your weapon')).toLowerCase();
-        const computerSelection = getComputerChoice();
-        result = playSingleRound(playerSelection, computerSelection);
-        if (result === "You win!") {
-            playerScore++;
-        } else if (result === "You lose!") {
-            computerScore++;
-        }
-        console.log (result);
-    }
-    if (playerScore > computerScore) {
-        return console.log("Player win")
-    } else if (playerScore < computerScore) {
-        return console.log("Computer win")
-    } else return console.log("Tie! Try again")
-}
-game();
+
+// function game () {
+//     let result;
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (i=1; i <= 5; i++) {
+//         const playerSelection = (prompt('Choose your weapon')).toLowerCase();
+//         const computerSelection = getComputerChoice();
+//         result = playSingleRound(playerSelection, computerSelection);
+//         if (result === "You win!") {
+//             playerScore++;
+//         } else if (result === "You lose!") {
+//             computerScore++;
+//         }
+//         console.log (result);
+//     }
+//     if (playerScore > computerScore) {
+//         return console.log("Player win")
+//     } else if (playerScore < computerScore) {
+//         return console.log("Computer win")
+//     } else return console.log("Tie! Try again")
+// }
+// game();
